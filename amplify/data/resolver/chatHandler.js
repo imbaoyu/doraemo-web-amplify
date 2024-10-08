99cc5de96f7506cdcd41f6ba9ac39cf3066359a3
Yu Bao <imbaoyu@gmail.com>
2024-10-07 23:10:37 -0700

    update chatHandler to use converse

--- amplify/data/resolver/chatHandler.js
+++ amplify/data/resolver/chatHandler.js
@@ -4,16 +4,26 @@ export function request(ctx) {
   const prompt = ctx.arguments.message;
 
   // https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_InvokeModel.html
+  // https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_Converse.html
   // model id: https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids.html#model-ids-arns
   return {
     method: "POST",
-    resourcePath: "/model/meta.llama3-70b-instruct-v1:0/invoke",
+    resourcePath: "/model/meta.llama3-70b-instruct-v1:0/converse",
     params: {
       body: JSON.stringify({
-        prompt: `Human: ${prompt}\nAssistant:`,
-        max_gen_len: 500,
-        temperature: 0.7,
-        top_p: 0.9,
+          messages: [ 
+              { 
+                 content: [{ text: `${prompt}\n`}],
+                 role: "user",
+              }
+          ],
+          inferenceConfig: {
+              maxTokens: 500,
+              stopSequences: [ "Human:", "Assistant:", "user:"],
+              temperature: 0.7,
+              topP: 0.9,
+          },
+          system: [ { text: "You are a flirty Scarlett Johansson, you try to flirt with the user when answering their questions" }],
       }),
       headers: {
         "Content-Type": "application/json",
@@ -26,8 +36,9 @@ export function request(ctx) {
 export function response(ctx) {
   console.log('Response result:', ctx.result);
   // Parse the response body
-  const responseBody = JSON.parse(ctx.result.body);
+  const responseBody = ctx.result.body;
+  return responseBody;
   // Extract the generated text from the response
-  const generatedText = responseBody.generation;
-  return generatedText.trim();
+  //const generatedText = responseBody.output.message.content[0].text;
+  //return generatedText.trim();
 }
