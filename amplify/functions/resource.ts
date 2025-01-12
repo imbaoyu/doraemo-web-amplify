04b7ed3f5a78d51494ef399be723f0233ed7fefe
Yu Bao <imbaoyu@gmail.com>
2025-01-12 15:08:36 -0800

    Changing ResourceGroupName for the document processor

--- amplify/functions/resource.ts
+++ amplify/functions/resource.ts
@@ -3,11 +3,13 @@ import { defineFunction } from "@aws-amplify/backend";
 export const chatWithBedrock = defineFunction({
     name: 'chatWithBedrock',
     entry: './chatHandler.ts',
-    timeoutSeconds: 30
+    timeoutSeconds: 30,
 });
 
+// See: https://docs.amplify.aws/react/build-a-backend/troubleshooting/circular-dependency/
 export const processDocument = defineFunction({
     name: 'processDocument',
+    resourceGroupName: 'data',
     entry: './storageHandler.ts',
     timeoutSeconds: 30
 }); 
