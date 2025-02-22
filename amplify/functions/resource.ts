0b5ca9a0facef723f0a67a0a149b07e3aa93a4b0
Yu Bao <imbaoyu@gmail.com>
2025-02-21 23:43:01 -0800

    fix amplify construct tree for the imported lambda

--- amplify/functions/resource.ts
+++ amplify/functions/resource.ts
@@ -1,13 +1,15 @@
 import { defineFunction } from "@aws-amplify/backend";
 import { Function } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
-export const chatWithBedrock = defineFunction(
-    (scope) => Function.fromFunctionArn(
-        scope,
+export const chatWithBedrock = defineFunction((scope: Construct) => {
+    const fn = new Construct(scope, 'chatWithBedrock');
+    return Function.fromFunctionArn(
+        fn,
         'Resource',
         'arn:aws:lambda:us-east-1:847373240038:function:DoraemoCdkStack-ChatProcessor34D41143-wOCdGd6rR0CK'
-    )
-);
+    );
+});
 
 // See: https://docs.amplify.aws/react/build-a-backend/troubleshooting/circular-dependency/
 export const processDocument = defineFunction({
@@ -15,3 +17,4 @@ export const processDocument = defineFunction({
     entry: './storageHandler.ts',
     timeoutSeconds: 30
 });
+ 
